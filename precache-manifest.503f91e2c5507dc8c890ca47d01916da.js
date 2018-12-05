20460d8b881e3dd40e4f1931df4b56e35cfb0972
ismyas <mattybroondog@gmail.com>
2018-12-04 20:54:05 -0500

    Updates

---
+++ precache-manifest.503f91e2c5507dc8c890ca47d01916da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be7c038d71beec2dab0b",
+    "url": "/siteweb/static/css/main.7a28282e.chunk.css"
+  },
+  {
+    "revision": "be7c038d71beec2dab0b",
+    "url": "/siteweb/static/js/main.be7c038d.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/siteweb/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "8827d30b251a45d80a4e",
+    "url": "/siteweb/static/js/runtime~main.8827d30b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/siteweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24bb152fe441f7ffabfdf05268590b71",
+    "url": "/siteweb/static/media/blueno-halftone-blue.24bb152f.gif"
+  },
+  {
+    "revision": "1bcf0adeb3f56217f56753323cb07d85",
+    "url": "/siteweb/index.html"
+  }
+];
