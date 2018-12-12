afb1a7b69a8332628baa72a862c8a707ebf42d96
ismyas <mattybroondog@gmail.com>
2018-12-12 01:45:06 -0500

    Revert "Updates"
    This reverts commit 20460d8b881e3dd40e4f1931df4b56e35cfb0972.

---
+++ artyouth/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <div className='frame'>
+      <div className='focal'>
+      <div className='title-image title-image-1'>
+      </div>
+      <div className='title-image title-image-2'>
+      </div>
+      <div className='title title-1'>
+      <span>GUERILLA<br/>ART<br/>YOUTH<br/>SCHOLARS</span>
+      </div>
+      </div>
+      </div>
+      <div className='frame frame-dark'>
+      <div className='title-link'>
+      <a href="mailto:mail@artyouth.com">contact us</a>
+      </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
